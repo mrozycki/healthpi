ebeeae99ef69f03f6d9c765b7d1e0e29b42dc03d
Mariusz Różycki <mar.rozycki@gmail.com>
2022-08-17 23:26:02 +0100

    Refactor backoff to be done in device factory

--- src/devices/device.rs
+++ src/devices/device.rs
@@ -1,9 +1,11 @@
+use std::collections::HashMap;
 use std::io::{BufRead, BufReader};
 use std::str::FromStr;
 use std::{collections::HashSet, error::Error, fs::File};
 
 use async_trait::async_trait;
-use bluez_async::{BluetoothSession, DeviceInfo, MacAddress};
+use bluez_async::{BluetoothSession, DeviceId, DeviceInfo, MacAddress};
+use chrono::{DateTime, Utc};
 use log::{debug, info, warn};
 
 use super::{contour, soehnle};
@@ -17,14 +19,54 @@ pub trait Device {
     fn get_device_info(&self) -> &DeviceInfo;
 }
 
+pub fn display_device(device_info: &DeviceInfo) -> String {
+    device_info
+        .name
+        .as_ref()
+        .unwrap_or(&device_info.mac_address.to_string())
+        .to_owned()
+}
+
+struct BackoffTable {
+    expiry_timestamps: HashMap<DeviceId, DateTime<Utc>>,
+}
+
+impl BackoffTable {
+    fn new() -> Self {
+        Self {
+            expiry_timestamps: HashMap::<DeviceId, DateTime<Utc>>::new(),
+        }
+    }
+
+    fn check(&self, device_id: &DeviceId) -> bool {
+        self.expiry_timestamps
+            .get(device_id)
+            .filter(|expiry| expiry > &&chrono::Utc::now())
+            .is_some()
+    }
+
+    fn mark(&mut self, device_id: &DeviceId) -> DateTime<Utc> {
+        let backoff_expiry = chrono::Utc::now()
+            .checked_add_signed(chrono::Duration::minutes(5))
+            .unwrap();
+        self.expiry_timestamps
+            .insert(device_id.clone(), backoff_expiry);
+        backoff_expiry
+    }
+}
+
 pub struct Factory {
     paired_devices: HashSet<MacAddress>,
+    backoff_table: BackoffTable,
 }
 
 impl Factory {
     #[allow(dead_code)]
     pub fn new(paired_devices: HashSet<MacAddress>) -> Self {
-        Self { paired_devices }
+        Self {
+            paired_devices,
+            backoff_table: BackoffTable::new(),
+        }
     }
 
     pub fn from_file(path: &str) -> std::io::Result<Self> {
@@ -38,13 +80,19 @@ impl Factory {
 
         info!("Loaded {} paired devices from file", paired_devices.len());
         debug!("Loaded devices: {:?}", paired_devices);
-        Ok(Self { paired_devices })
+        Ok(Self::new(paired_devices))
     }
 
     pub fn make_device(&self, device_info: DeviceInfo) -> Option<Box<dyn Device>> {
         if device_info.rssi.is_none() {
             None
         } else if !self.paired_devices.contains(&device_info.mac_address) {
+            None
+        } else if self.backoff_table.check(&device_info.id) {
+            debug!(
+                "Found device {}, ignoring because of backoff",
+                display_device(&device_info)
+            );
             None
         } else if let Some(name) = &device_info.name {
             if name.contains("Contour") {
@@ -63,5 +111,15 @@ impl Factory {
         } else {
             None
         }
+    }
+
+    pub fn mark_processed(&mut self, device: &dyn Device) -> DateTime<Utc> {
+        let expiry = self.backoff_table.mark(&device.get_device_info().id);
+        info!(
+            "Ignoring device {} until {}",
+            display_device(device.get_device_info()),
+            expiry
+        );
+        expiry
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,29 +5,20 @@ mod devices;
 mod store;
 
 use std::env;
+use std::error::Error;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::{Arc, Mutex};
 use std::time::Duration;
-use std::{collections::HashMap, error::Error};
 
-use bluez_async::{BluetoothSession, DeviceId, DiscoveryFilter, Transport};
-use chrono::{DateTime, Utc};
-use devices::device::{Device, Factory};
+use bluez_async::{BluetoothSession, DiscoveryFilter, Transport};
+use devices::device::Factory;
 use diesel::{Connection, SqliteConnection};
 use dotenv::dotenv;
 use log::{debug, error, info};
 use tokio::time;
 
+use crate::devices::device::display_device;
 use crate::store::db::measurement::MeasurementRepository;
-
-fn display_device(device: &dyn Device) -> String {
-    device
-        .get_device_info()
-        .name
-        .as_ref()
-        .unwrap_or(&device.get_device_info().mac_address.to_string())
-        .to_owned()
-}
 
 pub fn connect_to_database() -> Result<Arc<Mutex<SqliteConnection>>, Box<dyn Error>> {
     dotenv().ok();
@@ -46,7 +37,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     info!("Starting Bluetooth session");
     let (_, session) = BluetoothSession::new().await?;
-    let factory = Factory::from_file("devices.csv")?;
+    let mut factory = Factory::from_file("devices.csv")?;
 
     info!("Starting discovery");
     session
@@ -56,8 +47,6 @@ async fn main() -> Result<(), Box<dyn Error>> {
             ..Default::default()
         })
         .await?;
-
-    let mut backoff_table = HashMap::<DeviceId, DateTime<Utc>>::new();
 
     let running = Arc::new(AtomicBool::new(true));
     let running2 = running.clone();
@@ -69,27 +58,19 @@ async fn main() -> Result<(), Box<dyn Error>> {
         let mut devices = session.get_devices().await?.into_iter();
 
         while let Some(device) = devices.next().and_then(|x| factory.make_device(x)) {
-            if backoff_table
-                .get(&device.get_device_info().id)
-                .filter(|expiry| expiry > &&chrono::Utc::now())
-                .is_some()
-            {
-                debug!(
-                    "Found device {}, ignoring because of backoff",
-                    display_device(device.as_ref())
-                );
-                continue;
-            }
             info!(
                 "Found device {}, connecting",
-                display_device(device.as_ref())
+                display_device(device.get_device_info())
             );
             if let Err(e) = device.connect(&session).await {
                 error!("Failed to connect, skipping: {:?}", e);
                 continue;
             }
 
-            info!("Getting data from {}", display_device(device.as_ref()));
+            info!(
+                "Getting data from {}",
+                display_device(device.get_device_info())
+            );
             match device.get_data(&session).await {
                 Ok(records) => {
                     info!("Fetched {} records", records.len());
@@ -108,16 +89,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
                         error!("Failed to store records in database, skipping. {}", e);
                         continue;
                     }
-
-                    let backoff_expiry = chrono::Utc::now()
-                        .checked_add_signed(chrono::Duration::minutes(5))
-                        .unwrap();
-                    info!(
-                        "Ignoring device {} until {}",
-                        display_device(device.as_ref()),
-                        backoff_expiry
-                    );
-                    backoff_table.insert(device.get_device_info().id.clone(), backoff_expiry);
+                    factory.mark_processed(device.as_ref());
                 }
                 Err(e) => error!("Failed to get data: {:?}", e),
             }
