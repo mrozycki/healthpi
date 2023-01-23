a37087ead221e697793eef24a3efc12d230c419a
Mariusz Różycki <mar.rozycki@gmail.com>
2023-01-23 22:20:16 +0100

    Add enum for value types

--- healthpi-db/src/measurement.rs
+++ healthpi-db/src/measurement.rs
@@ -1,6 +1,22 @@
 use chrono::NaiveDateTime;
 use healthpi_bt::MacAddress;
+use num::FromPrimitive;
 use num_derive::FromPrimitive;
+
+#[derive(Debug, FromPrimitive, PartialEq)]
+pub enum ValueType {
+    Weight,
+    BodyMassIndex,
+    BasalMetabolicRate,
+    WaterPercent,
+    MusclePercent,
+    FatPercent,
+    Glucose,
+    Meal,
+    BloodPressureSystolic,
+    BloodPressureDiastolic,
+    HeartRate,
+}
 
 #[derive(Debug, FromPrimitive, PartialEq)]
 pub enum MealIndicator {
@@ -29,17 +45,21 @@ pub enum Value {
 impl Into<(usize, f64)> for Value {
     fn into(self) -> (usize, f64) {
         match self {
-            Value::Weight(x) => (0, x),
-            Value::BodyMassIndex(x) => (1, x),
-            Value::BasalMetabolicRate(x) => (2, x),
-            Value::WaterPercent(x) => (3, x),
-            Value::MusclePercent(x) => (4, x),
-            Value::FatPercent(x) => (5, x),
-            Value::Glucose(x) => (6, x as f64),
-            Value::Meal(x) => (7, x as u8 as f64),
-            Value::BloodPressureSystolic(x) => (8, x as f64),
-            Value::BloodPressureDiastolic(x) => (9, x as f64),
-            Value::HeartRate(x) => (10, x as f64),
+            Value::Weight(x) => (ValueType::Weight as usize, x),
+            Value::BodyMassIndex(x) => (ValueType::BodyMassIndex as usize, x),
+            Value::BasalMetabolicRate(x) => (ValueType::BasalMetabolicRate as usize, x),
+            Value::WaterPercent(x) => (ValueType::WaterPercent as usize, x),
+            Value::MusclePercent(x) => (ValueType::MusclePercent as usize, x),
+            Value::FatPercent(x) => (ValueType::FatPercent as usize, x),
+            Value::Glucose(x) => (ValueType::Glucose as usize, x as f64),
+            Value::Meal(x) => (ValueType::Meal as usize, x as u8 as f64),
+            Value::BloodPressureSystolic(x) => {
+                (ValueType::BloodPressureSystolic as usize, x as f64)
+            }
+            Value::BloodPressureDiastolic(x) => {
+                (ValueType::BloodPressureDiastolic as usize, x as f64)
+            }
+            Value::HeartRate(x) => (ValueType::HeartRate as usize, x as f64),
         }
     }
 }
@@ -48,21 +68,21 @@ impl TryFrom<(usize, f64)> for Value {
     type Error = &'static str;
 
     fn try_from((index, x): (usize, f64)) -> Result<Self, Self::Error> {
-        match index {
-            0 => Ok(Value::Weight(x)),
-            1 => Ok(Value::BodyMassIndex(x)),
-            2 => Ok(Value::BasalMetabolicRate(x)),
-            3 => Ok(Value::WaterPercent(x)),
-            4 => Ok(Value::MusclePercent(x)),
-            5 => Ok(Value::FatPercent(x)),
-            6 => Ok(Value::Glucose(x as i32)),
-            7 => num::FromPrimitive::from_f64(x)
+        match ValueType::from_usize(index) {
+            Some(ValueType::Weight) => Ok(Value::Weight(x)),
+            Some(ValueType::BodyMassIndex) => Ok(Value::BodyMassIndex(x)),
+            Some(ValueType::BasalMetabolicRate) => Ok(Value::BasalMetabolicRate(x)),
+            Some(ValueType::WaterPercent) => Ok(Value::WaterPercent(x)),
+            Some(ValueType::MusclePercent) => Ok(Value::MusclePercent(x)),
+            Some(ValueType::FatPercent) => Ok(Value::FatPercent(x)),
+            Some(ValueType::Glucose) => Ok(Value::Glucose(x as i32)),
+            Some(ValueType::Meal) => num::FromPrimitive::from_f64(x)
                 .map(|v| Value::Meal(v))
                 .ok_or("Invalid meal indicator"),
-            8 => Ok(Value::BloodPressureSystolic(x as i32)),
-            9 => Ok(Value::BloodPressureDiastolic(x as i32)),
-            10 => Ok(Value::HeartRate(x as i32)),
-            _ => Err("Invalid type"),
+            Some(ValueType::BloodPressureSystolic) => Ok(Value::BloodPressureSystolic(x as i32)),
+            Some(ValueType::BloodPressureDiastolic) => Ok(Value::BloodPressureDiastolic(x as i32)),
+            Some(ValueType::HeartRate) => Ok(Value::HeartRate(x as i32)),
+            None => Err("Invalid value type"),
         }
     }
 }
