0f7e8c2c28791d20c72c24dbca4db63729a56e96
Mariusz Różycki <mar.rozycki@gmail.com>
2023-02-11 00:28:24 +0100

    Support query by value type

--- healthpi-api/src/main.rs
+++ healthpi-api/src/main.rs
@@ -1,13 +1,43 @@
+use std::str::FromStr;
+
 use actix_web::{get, web, App, HttpServer, Responder};
-use healthpi_db::db::{
-    connection::Connection,
-    measurement::{MeasurementRepository, MeasurementRepositoryImpl},
+use healthpi_db::{
+    db::{
+        connection::Connection,
+        measurement::{MeasurementRepository, MeasurementRepositoryImpl},
+    },
+    measurement::ValueType,
 };
 use log::info;
+use serde::{de, Deserialize};
+
+fn comma_separated_value_types<'de, D>(deserializer: D) -> Result<Vec<ValueType>, D::Error>
+where
+    D: de::Deserializer<'de>,
+{
+    let s: &str = Deserialize::deserialize(deserializer)?;
+    s.split(",")
+        .map(|s| ValueType::from_str(s).map_err(|e| de::Error::custom(e.to_string())))
+        .collect()
+}
+
+#[derive(Debug, Deserialize)]
+struct Query {
+    #[serde(deserialize_with = "comma_separated_value_types")]
+    select: Vec<ValueType>,
+}
 
 #[get("/")]
-async fn index(measurement_repository: web::Data<MeasurementRepositoryImpl>) -> impl Responder {
-    web::Json(measurement_repository.fetch_records().await.unwrap())
+async fn index(
+    measurement_repository: web::Data<MeasurementRepositoryImpl>,
+    query: web::Query<Query>,
+) -> impl Responder {
+    web::Json(
+        measurement_repository
+            .fetch_records(&query.select)
+            .await
+            .unwrap(),
+    )
 }
 
 #[actix_web::main]
