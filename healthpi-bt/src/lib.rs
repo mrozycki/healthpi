759fdd4ace6e3db23b3aa763fe3e14e4e50bb49a
Mariusz Różycki <mar.rozycki@gmail.com>
2024-03-20 21:28:00 +0100

    Remove bluez

--- healthpi-bt/src/lib.rs
+++ healthpi-bt/src/lib.rs
@@ -1,10 +1,4 @@
-#[cfg(all(feature = "bluez", feature = "btleplug"))]
-compile_error!("\"bluez\" and \"btleplug\" cannot be used at the same time");
-
 mod api;
-#[cfg(feature = "bluez")]
-mod bluez;
-#[cfg(feature = "btleplug")]
 mod btleplug;
 mod macaddress;
 
@@ -12,8 +6,5 @@ pub use api::{
     BleCharacteristic, BleCharacteristicEvent, BleDevice, BleSession, DeviceError, DeviceId,
     MockBleCharacteristic, MockBleDevice, MockBleSession,
 };
-#[cfg(feature = "bluez")]
-pub use bluez::create_session;
-#[cfg(feature = "btleplug")]
 pub use btleplug::create_session;
 pub use macaddress::MacAddress;
